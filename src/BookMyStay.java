05b45fb7e9ad6959e532f38a2f974958a8d64fe4
Sai-Sutharsanan05 <saisutharsanan@gmail.com>
2026-03-10 09:17:26 +0530

    Base application setup

---
+++ src/BookMyStay.java
@@ -0,0 +1,6 @@
+public class BookMyStay {
+    public static void main(String[] args){
+        System.out.println("welcome to the Hotel Management System");
+        System.out.println("System initialized successfully");
+    }
+}
