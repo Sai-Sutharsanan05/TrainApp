c648f99e4cdf0a52431c7c4cb7397f1b59ef5576
Sai Sutharsanan <saisutharsanan@gmail.com>
2026-04-02 23:10:44 +0530

    UC5: Booking Request

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,6 +1,56 @@
-void main() {
-    IO.println(String.format("Hello and welcome!"));
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Primary Class: BookMyStayApp (Use Case 5)
+ * Version: 5.0 - Booking Request Queue (FIFO)
+ */
+public class BookMyStay{
+    public static void main(String args[]) {
+        System.out.println("Welcome to BookMyStayApp");
+        System.out.println("Version: 5.0 - Booking Request Queue\n");
+
+        BookingRequestQueue requestQueue = new BookingRequestQueue();
+
+        // Adding requests (Kishore -> Arun -> Divya)
+        requestQueue.addRequest(new Reservation("Kishore", "Single"));
+        requestQueue.addRequest(new Reservation("Arun", "Suite"));
+        requestQueue.addRequest(new Reservation("Divya", "Double"));
+
+        // Displaying the queue to confirm FIFO order
+        requestQueue.displayQueue();
+
+        System.out.println("\nNo allocation done yet.");
+    }
+}
+
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest: " + guestName + ", Requested Room: " + roomType;
+    }
+}
+
+class BookingRequestQueue {
+    private Queue<Reservation> queue = new LinkedList<>();
+
+    public void addRequest(Reservation reservation) {
+        queue.offer(reservation);
+        System.out.println("Request added: " + reservation);
+    }
+
+    public void displayQueue() {
+        System.out.println("\nCurrent Booking Queue (FIFO Order):");
+        for (Reservation r : queue) {
+            System.out.println(r);
+        }
     }
 }
