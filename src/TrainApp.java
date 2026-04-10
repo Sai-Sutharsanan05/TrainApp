2ca8bd5eb79863632855fdb61c66b4954925eee9
Sai-Sutharsanan05 <saisutharsanan@gmail.com>
2026-04-10 10:22:16 +0530

    UC3: Track Unique Bogie IDs

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,21 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
-public class TrainApp {
+import java.util.HashSet;
+import java.util.Set;
+
+public class TrainApp{
+
     public static void main(String[] args) {
-        System.out.println("UC2 Add Passenger Bogies to Train\n");
-        List<String> passengerBogies = new ArrayList<>();
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogies: " + passengerBogies);
-        System.out.println("\nAfter Removing 'AC Chair':");
-        passengerBogies.remove("AC Chair");
-        System.out.println("Passenger Bogies: " + passengerBogies);
-        System.out.println("\nChecking if 'Sleeper' exists:");
-        boolean hasSleeper = passengerBogies.contains("Sleeper");
-        System.out.println("Contains Sleeper?: " + hasSleeper);
-        System.out.println("\nFinal Train Passenger Consist: " + passengerBogies);
-        System.out.println("\nUC2 operations completed successfully..");
+
+        System.out.println("====================================");
+        System.out.println(" UC3: Track Unique Bogie IDs ");
+        System.out.println("====================================");
+
+        // Create a Set to store unique bogie IDs
+        // HashSet stores only unique values
+        Set<String> bogies = new HashSet<>();
+
+        // Adding bogie IDs (including duplicates)
+        // add() inserts bogie IDs into the set
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+
+        // Duplicate entries (will be ignored automatically)
+        bogies.add("BG101"); // Duplicate entry
+        bogies.add("BG102"); // Duplicate entry
+
+        // Display unique bogie IDs
+        System.out.println("\nUnique Bogie IDs in Train Consist:");
+        for (String bogie : bogies) {
+            System.out.println(bogie);
+        }
+
+        System.out.println("\nTotal Unique Bogies: " + bogies.size());
     }
 }
