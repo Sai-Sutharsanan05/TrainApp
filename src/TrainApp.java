a1793dd0f80ffc3319558328327ded9006b60c27
Sai-Sutharsanan05 <saisutharsanan@gmail.com>
2026-04-10 11:02:31 +0530

    UC4: Maintain Ordered Bogie IDs

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,35 +1,22 @@
-import java.util.HashSet;
-import java.util.Set;
-
-public class TrainApp{
-
+import java.util.LinkedList;
+public class UseCase4TrainConsistApp {
     public static void main(String[] args) {
-
         System.out.println("====================================");
-        System.out.println(" UC3: Track Unique Bogie IDs ");
+        System.out.println(" UC4: Maintain Ordered Bogie IDs ");
         System.out.println("====================================");
-
-        // Create a Set to store unique bogie IDs
-        // HashSet stores only unique values
-        Set<String> bogies = new HashSet<>();
-
-        // Adding bogie IDs (including duplicates)
-        // add() inserts bogie IDs into the set
-        bogies.add("BG101");
-        bogies.add("BG102");
-        bogies.add("BG103");
-        bogies.add("BG104");
-
-        // Duplicate entries (will be ignored automatically)
-        bogies.add("BG101"); // Duplicate entry
-        bogies.add("BG102"); // Duplicate entry
-
-        // Display unique bogie IDs
-        System.out.println("\nUnique Bogie IDs in Train Consist:");
-        for (String bogie : bogies) {
+        LinkedList<String> train = new LinkedList<>();
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC Chair");
+        train.add("Cargo");
+        train.add("Guard");
+        train.add(2, "Pantry Car");
+        train.removeFirst(); // Removes Engine
+        train.removeLast();  // Removes Guard
+        System.out.println("\nFinal Train Consist:");
+        for (String bogie : train) {
             System.out.println(bogie);
         }
-
-        System.out.println("\nTotal Unique Bogies: " + bogies.size());
+        System.out.println("\nTotal Bogies: " + train.size());
     }
 }
